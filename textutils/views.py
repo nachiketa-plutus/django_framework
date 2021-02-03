3056dcae37716177909d3e4e0713081ef57b515d
nachiketa-plutus <77604049+nachiketa-plutus@users.noreply.github.com>
2021-02-03 12:44:53 +0530

    Update views.py

--- textutils/views.py
+++ textutils/views.py
@@ -1,82 +1,75 @@
-'''Short documentation for functions defined.'''
-# This has been created by Nachiketa Buddha!
 from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def Home(request):
+def index(request):
     return render(request, 'index.html')
 
-    # return HttpResponse("<h2>This will very shortly display the HOME section of the textutils project <br><br>"
-    #                     "And hello! This is Nachiketa! "
-    #                     '''<br><br> <button onclick="document.location='http://127.0.0.1:8000/About'"> <strong><h2>About<h2></strong> </button></h2>'''
-    #                     '''<hr><button onclick="document.location='http://127.0.0.1:8000/removepunctuation'"> <strong><h2>Remove Punctuation Page<h2></strong> </button>&nbsp&nbsp&nbsp</h2>'''
-    #                     '''<button onclick="document.location='http://127.0.0.1:8000/capitalizefirst'"> <strong><h2>Capitalize First Letter Page<h2></strong> </button>&nbsp&nbsp&nbsp</h2>'''
-    #                     '''<button onclick="document.location='http://127.0.0.1:8000/removespace'"> <strong><h2>Remove Spaces Page<h2></strong> </button></h2>&nbsp&nbsp&nbsp'''
-    #                     '''<button onclick="document.location='http://127.0.0.1:8000/newlineremove'"> <strong><h2>Remove New Lines Page<h2></strong> </button></h2>&nbsp&nbsp&nbsp'''
-    #                     '''<button onclick="document.location='http://127.0.0.1:8000/newlineremove'"> <strong><h2>Characters Count Page<h2></strong> </button></h2><br><br>''')
+    # return HttpResponse("Home")
 
-def about(request):
-    return HttpResponse("<h2>This is an about page for textutils project by Nachiketa!</h2> <br><br> "
-                        '''<button onclick="document.location='http://127.0.0.1:8000'"><strong><h2>Back to Main Page<h2></strong></button></h2>''')
 
 def analyze(request):
-    #Getting text
-    djangoText=print(request.GET.get('text','default'))
+    #Get the text
+    djtext = request.GET.get('text', 'default')
+    print(djtext)
 
-    #redirecting to analyze page of the project.
-    return render(request, 'analyze.html')
+    # Check checkbox values
+    removepunc = request.GET.get('removepunc', 'off')
+    fullcaps = request.GET.get('fullcaps', 'off')
+    newlineremover = request.GET.get('newlineremover', 'off')
+    extraspaceremover = request.GET.get('extraspaceremover', 'off')
 
-def remove_punctuation(request):
-    return HttpResponse("<h2>This is the page for removing punctuations in the textutils project by Nachiketa! </h2> <br><br> "
-                        '''<button onclick="document.location='http://127.0.0.1:8000/'"><strong><h2>Back to Main Page<h2></strong></button></h2>''')
+    #Check which checkbox is on
+    if removepunc == "on":
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        analyzed = ""
+        for char in djtext:
+            if char not in punctuations:
+                analyzed = analyzed + char
 
-def capitalize_first(request):
-    return HttpResponse("<h2>This is the page to capitalize first letter of each word in the textutils project by Nachiketa! </h2> <br><br> "
-                        '''<button onclick="document.location='http://127.0.0.1:8000'"><strong><h2>Back to Main Page<h2></strong></button></h2>''')
+        params = {'purpose':'Removed Punctuations', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
 
-def remove_space(request):
-    return HttpResponse("<h2>This is the page to remove spaces in the textutils project by Nachiketa! <br><br> </h2>"
-                        '''<button onclick="document.location='http://127.0.0.1:8000/'"><strong><h2>Back to Main Page<h2></strong></button></h2>''')
+    elif(fullcaps=="on"):
+        analyzed = ""
+        for char in djtext:
+            analyzed = analyzed + char.upper()
 
-def newline_remove(request):
-    return HttpResponse("<h2>This is the page to remove a newer line in the textutils project by Nachiketa! <br><br> </h2>"
-                        '''<button onclick="document.location='http://127.0.0.1:8000'"><strong><h2>Back to Main Page<h2></strong></button></h2>''')
+        params = {'purpose': 'Changed to Uppercase', 'analyzed_text': analyzed}
+        # Analyze the text
+        return render(request, 'analyze.html', params)
 
-def char_count(request):
-    return HttpResponse("<h2>This is the page to count characters wherever required in the textutils project by Nachiketa! <br><br> </h2>"
-                        '''<button onclick="document.location='http://127.0.0.1:8000/'"><strong><h2>Back to Main Page<h2></strong></button></h2>''')
+    elif(extraspaceremover=="on"):
+        analyzed = ""
+        for index, char in enumerate(djtext):
+            if not(djtext[index] == " " and djtext[index+1]==" "):
+                analyzed = analyzed + char
 
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+        # Analyze the text
+        return render(request, 'analyze.html', params)
 
+    elif (newlineremover == "on"):
+        analyzed = ""
+        for char in djtext:
+            if char != "\n":
+                analyzed = analyzed + char
 
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+        # Analyze the text
+        return render(request, 'analyze.html', params)
+    else:
+        return HttpResponse("Error")
 
-
-
-
+# def capfirst(request):
+#     return HttpResponse("capitalize first")
+#
+# def newlineremove(request):
+#     return HttpResponse("newline remove first")
 #
 #
+# def spaceremove(request):
+#     return HttpResponse("space remover back")
 #
-# # def links(request):
-# #     return HttpResponse('''<h1>Personal Navigator for some of the top few websites I surf.</h1>
-# #                         <a href="https://www.programiz.com/python-programming/file-operation" target="_blank">
-# #                         File Operation on Programmiz </a><br><br>
-# #                         <a href="https://www.geeksforgeeks.org/open-a-file-in-python/" target="_blank">
-# #                         Visit GeeksForGeeks for File Operations in Python. </a>''')
-#
-# '''To open an existing file on the django server or website'''
-# '''
-# ----------------------------------------
-# Pass the content variable defined above,
-# to read the existing file named myFile.txt. Path is passed too.
-# -------------------------------------------
-# '''
-# # def file(request):
-# #     f=open("C:/New folder/myFile.txt")
-# #     content1=f.read()
-# #     return HttpResponse(content1)
-# # def file2(request):
-# #     with open("C:/New folder/myFile1.txt") as file:
-# #         content=file.read()
-# #     return HttpResponse(content)
-#
-#
+# def charcount(request):
+#     return HttpResponse("charcount ")
